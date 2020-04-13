2c27e64e4e89bda6f6f3fb9ed13454a7c654f3de
codekid <ckrackerx15@gmail.com>
2020-04-13 23:41:27 +0530

    add probe comment and example

--- probe.go
+++ probe.go
@@ -5,7 +5,19 @@ import (
 	"time"
 )
 
-// GetTestClient returns a probed client for testing
+// GetTestClient returns a probed client for testing your rubik server
+//
+// Example:
+// 		var client *rubik.Client
+// 		func init() {
+//			routers.import()
+//			client = rubik.GetTestClient()
+// }
+//
+// 		func TestSomething(t *testing.T) {
+//			resp, err := client.Get("/")
+//			** TEST YOUR `resp` **
+//}
 func GetTestClient() *Client {
 	s := httptest.NewServer(app.mux)
 	return NewClient(s.URL, time.Second*60)
