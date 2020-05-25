f8e947e3b7a89ca68d89f383d195e8aec6068239
codekid <ckrackerx15@gmail.com>
2020-05-25 20:05:04 +0530

    replc: help info of repl removed until 1.0 release

--- replc/help.go
+++ replc/help.go
@@ -11,40 +11,50 @@ type cmd struct {
 }
 
 var allCommands = []cmd{
-	cmd{
+	{
 		name:  "create",
 		desc:  "creates a new rubik project",
 		usage: "okrubik create",
 	},
-	cmd{
+	{
 		name:  "run",
 		desc:  "runs project from your rubik workspace",
 		usage: "okrubik run",
 	},
+	{
+		name:  "x",
+		desc:  "executes a defined command from your rubik workspace",
+		usage: "okrubik x:test",
+	},
+	{
+		name:  "gen",
+		desc:  "generates boilerplate code for your rubik project",
+		usage: "okrubik gen router",
+	},
 }
 
 var replCommands = []cmd{
-	cmd{
+	{
 		name:  "select",
 		desc:  "selects the project that you have initialized inside rubik.toml",
 		usage: "select [project_name]",
 	},
-	cmd{
+	{
 		name:  "about",
 		desc:  "shows the requirement and description of a route",
 		usage: "about [full_route]",
 	},
-	cmd{
+	{
 		name:  "list",
 		desc:  "lists all route that have been added to rubik server",
 		usage: "list",
 	},
-	cmd{
+	{
 		name:  "find",
 		desc:  "finds the route that likely matches your argument",
 		usage: "find [route]",
 	},
-	cmd{
+	{
 		name:  "exit",
 		desc:  "exit the okrubik repl",
 		usage: "exit",
@@ -67,18 +77,14 @@ func HelpCommand(args []string) string {
 Tooling Commands:
 
 %s
-
-REPL Commands:
-
-%s
-	`
+`
 	var cmdStr string
-	var replStr string
+	// var replStr string
 	for _, c := range allCommands {
 		cmdStr += fmt.Sprintf("%s 		 %s - %s\n", c.name, c.desc, c.usage)
 	}
-	for _, c := range replCommands {
-		replStr += fmt.Sprintf("%s 		 %s - %s\n", c.name, c.desc, c.usage)
-	}
-	return fmt.Sprintf(skeleton, cmdStr, replStr)
+	// for _, c := range replCommands {
+	// 	replStr += fmt.Sprintf("%s 		 %s - %s\n", c.name, c.desc, c.usage)
+	// }
+	return fmt.Sprintf(skeleton, cmdStr)
 }
