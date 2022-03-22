96af4f3b8ff7cba3d8c63e32c9030bae2917dc70
removedporn <86824510+removedporn@users.noreply.github.com>
2022-03-22 16:53:58 +0800

    Delete entity.go

--- entity.go
+++
@@ -1,9 +0,0 @@
-package rubik
-
-type punchInEn struct {
-	Entity
-	Workspace string `rubik:"body"`
-	Service   string `rubik:"body"`
-	Host      string `rubik:"body"`
-	Port      string `rubik:"body"`
-}
